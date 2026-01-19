e1740520cdd36bc0b9c95086813f295893131346
facundo iglesias <facundoiglesias9@gmail.com>
2026-01-19 00:57:17 -0300

    fix: restore types.ts in plain utf8

---
+++ src/utils/supabase/types.ts
@@ -0,0 +1,2 @@
+export type Json = string | number | boolean | null | { [key: string]: Json | undefined } | Json[]
+export type Database = { public: { Tables: { [_ in string]: { Row: { [_ in string]: any }; Insert: { [_ in string]: any }; Update: { [_ in string]: any } } }; Views: { [_ in string]: any }; Functions: { [_ in string]: any }; Enums: { [_ in string]: any }; CompositeTypes: { [_ in string]: any } } }
